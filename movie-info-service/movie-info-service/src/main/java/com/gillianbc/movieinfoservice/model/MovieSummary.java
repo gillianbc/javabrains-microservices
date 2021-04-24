43da5864c7a003fef32ec5b934aded15a5b65a47
gillianbc <gszemeti@gmail.com>
2021-04-25 00:26:02 +0100

    work in progress

---
+++ movie-info-service/movie-info-service/src/main/java/com/gillianbc/movieinfoservice/model/MovieSummary.java
@@ -0,0 +1,35 @@
+package com.gillianbc.movieinfoservice.model;
+
+public class MovieSummary {
+  private String movieId;
+  private String name;
+
+  public MovieSummary(final String movieId, final String name) {
+    this.movieId = movieId;
+    this.name = name;
+  }
+
+  public String getMovieId() {
+    return movieId;
+  }
+
+  public void setMovieId(final String movieId) {
+    this.movieId = movieId;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(final String name) {
+    this.name = name;
+  }
+
+  @Override
+  public String toString() {
+    return "Movie{" +
+        "movieId='" + movieId + '\'' +
+        ", name='" + name + '\'' +
+        '}';
+  }
+}
--- movie-info-service/movie-info-service/src/main/java/com/gillianbc/movieinfoservice/resource/MovieService.java
+++ movie-info-service/movie-info-service/src/main/java/com/gillianbc/movieinfoservice/resource/MovieService.java
@@ -1,6 +1,7 @@
 package com.gillianbc.movieinfoservice.resource;
 
 import com.gillianbc.movieinfoservice.model.Movie;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/movies")
 public class MovieService {
+
+    // Bring in property value from our application.properties
+    @Value("api-key")
+    private String apiKey;
 
     @RequestMapping("/{movieId}")
     public Movie getMovieInfo(@PathVariable("movieId") String movieId){
