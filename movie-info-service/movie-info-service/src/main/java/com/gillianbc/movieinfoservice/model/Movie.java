6a7a601aa079d59b41b9020d136afb5594aec3d2
gillianbc <gszemeti@gmail.com>
2021-04-25 22:39:51 +0100

    Fixed build issues

--- movie-info-service/movie-info-service/src/main/java/com/gillianbc/movieinfoservice/model/Movie.java
+++ movie-info-service/movie-info-service/src/main/java/com/gillianbc/movieinfoservice/model/Movie.java
@@ -2,11 +2,13 @@ package com.gillianbc.movieinfoservice.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Data
 @ToString
 @AllArgsConstructor
+@NoArgsConstructor
 public class Movie {
   private String movieId;
   private String name;
